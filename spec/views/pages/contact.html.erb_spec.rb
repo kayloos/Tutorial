bfa9e4f735f3abf03f82229f2e0dde8afe692fdd
Kayloos <kalusn@gmail.com>
2010-01-17 23:02:16 +0100

    Done with the static pages

---
+++ spec/views/pages/contact.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "/pages/contact" do
+  before(:each) do
+    render 'pages/contact'
+  end
+
+  #Delete this example and add some real ones or delete this file
+end
---
+++ spec/views/pages/home.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "/pages/home" do
+  before(:each) do
+    render 'pages/home'
+  end
+
+  #Delete this example and add some real ones or delete this file
+end
